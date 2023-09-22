0b503c120e0c944d5d633a2f24c3b0cdedb58e99
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-09-22 22:13:20 +0200

    Update script_analyse2.R

--- script_analyse2.R
+++ script_analyse2.R
@@ -2,6 +2,8 @@
 
 #setwd(your path) #get it with getwd() function in your R shell, non needed for unix based systems
 
+# !!! This is a configuration exmaple, please update to your needs following README.md !!!
+
 pattern_to_be <- "_extr.csv"
 
 sep_ = ','
